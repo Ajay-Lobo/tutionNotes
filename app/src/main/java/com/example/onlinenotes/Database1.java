c6c9ea9097674809d00b74edd77594dfea47664f
AJAY N LOBO <nishanthajay1101@gmail.com>
2023-08-27 19:35:54 +0530

    second edit

--- app/src/main/java/com/example/onlinenotes/Database1.java
+++ app/src/main/java/com/example/onlinenotes/Database1.java
@@ -1,5 +1,6 @@
 package com.example.onlinenotes;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -18,13 +19,40 @@ public class Database1 extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("" +
                 "CREATE TABLE IF NOT EXISTS " + register +
-                "(usn INT PRIMARY KEY, " +
+                "(usn TEXT PRIMARY KEY, " +
+                "name TEXT NOT NULL, " +
                 "password TEXT NOT NULL, " +
                 "role NUMBER DEFAULT 1 " +
                 ")"
         );
-    }
 
+        sqLiteDatabase.execSQL("" +
+                "INSERT INTO " + register + " " +
+                "(usn,name, password, role) VALUES " +
+                "('4SO22MC0091', 'harish','123456', 0)"
+        );
+    }
+    public boolean registerData(String name,String usn, String password) {
+
+        //open database in writable mode to insert data
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        //Create contentValues; a string-builder-like function
+        ContentValues contentValues = new ContentValues();
+
+
+        contentValues.put("name", name);
+        contentValues.put("usn", usn);
+        contentValues.put("password", password);
+
+
+        //insert data into table
+        long res = sqLiteDatabase.insert(register, null, contentValues);
+
+        sqLiteDatabase.close();
+        return res != -1;
+
+    }
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
--- app/src/main/java/com/example/onlinenotes/MainActivity.java
+++ app/src/main/java/com/example/onlinenotes/MainActivity.java
@@ -7,41 +7,76 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-     EditText usernameEditText,passwordEditText;
+     EditText usernameEditText,passwordEditText1,passwordEditText2,name1;
+     TextView loginPress;
     Button loginButton;
+    Database1 database1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        usernameEditText = findViewById(R.id.editTextText);
-        passwordEditText = findViewById(R.id.editTextDate);
+        name1 = findViewById(R.id.editTextText);
+        usernameEditText= findViewById(R.id.editTextName);
+        passwordEditText1 = findViewById(R.id.editTextPass1);
+        passwordEditText2 = findViewById(R.id.editTextPass2);
+        loginPress = findViewById(R.id.LoginView2);
+
         loginButton = findViewById(R.id.loginButton);
+//        database calling
+        database1 = new Database1(getApplicationContext());
+        //linking another page
+        loginPress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Get username and password from EditTexts
+                String name = name1.getText().toString();
                 String username = usernameEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
+                String password = passwordEditText1.getText().toString();
+                String confirmPassword = passwordEditText2.getText().toString();
+                    if(name.length()<4) {
+                        //error messages
+                        Toast.makeText(getApplicationContext(),"Name is Short",Toast.LENGTH_SHORT).show();
+                    }
+                    else if(password.length()<6){
+                        Toast.makeText(getApplicationContext(),"Password is Short",Toast.LENGTH_SHORT).show();
+
+                    }
+               else if(!password.equalsIgnoreCase(confirmPassword)){
+                        Toast.makeText(getApplicationContext(),"Password doesn't match",Toast.LENGTH_SHORT).show();
+                    }
+               else {
+                        if (database1.registerData(name, username, password)) {
+                            Toast.makeText(getApplicationContext(), "Registered Successfully", Toast.LENGTH_SHORT).show();
+                            navigateToLogin();
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "Registered Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    }
+
 
                 // Perform login validation (example: check against hardcoded credentials)
-                if (isValidCredentials(username, password)) {
-                    navigateToDashboard();
-                    // Successful login action (e.g., navigate to another activity)
-                } else {
-                    // Handle unsuccessful login (e.g., show an error message)
-                }
-            }
 
-            private boolean isValidCredentials(String username, String password) {
-                return username.equals("your_username") && password.equals("your_password");
-            }
+
+
         });
     }
 
-    private void navigateToDashboard() {
-        Intent intent = new Intent(MainActivity.this, .class);
+    private void navigateToLogin() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(intent);
         finish();
     }
